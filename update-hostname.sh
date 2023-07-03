db3d66f16c64a40ae0787a046f8bb54e7a38f476
Panji Tri Atmojo <panji.atmojo@gmail.com>
2023-07-03 11:55:14 +0700

    Fixed hosts name update

--- update-hostname.sh
+++ update-hostname.sh
@@ -7,5 +7,5 @@ read -p 'New Hostname: ' new_hostname
 mv /etc/hostname /etc/hostname.backup
 echo "$new_hostname" > /etc/hostname
 
-mv /etc/hosts /etc/hosts.backup
-printf '%s\n%s\n' "127.0.0.1 $new_hostname"  "$(cat /etc/hosts)" > /etc/hosts
+cp /etc/hosts /etc/hosts.backup
+sed -i 's/'$current_hostname'/'$new_hostname'/g' /etc/hosts 

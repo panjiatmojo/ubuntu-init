13db6aa66a62140e13136293d17d1bc1943c1718
Panji Tri Atmojo <panji.atmojo@gmail.com>
2023-07-03 12:18:42 +0700

    Fixed new command termination

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -7,5 +7,4 @@ ssh-keygen -t rsa -b 4096 -m PEM && \
 cp ./.ssh/id_rsa.pub ./.ssh/authorized_keys && \
 chown jenkins:jenkins ./.ssh/authorized_keys && \
 cat ./.ssh/id_rsa && \
-echo 'jenkins ALL = (root) NOPASSWD: /usr/bin/git, /usr/bin/docker-compose, /usr/bin/chmod' >> /etc/sudoers.d/jenkins-levero && \
-
+echo 'jenkins ALL = (root) NOPASSWD: /usr/bin/git, /usr/bin/docker-compose, /usr/bin/chmod' >> /etc/sudoers.d/jenkins-levero

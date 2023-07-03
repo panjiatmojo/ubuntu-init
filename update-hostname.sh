00b24f6912e38f0c1686b73215ddd8a4b38b1aeb
Panji Tri Atmojo <panji.atmojo@gmail.com>
2023-07-03 12:18:16 +0700

    Added Leave empty hostname

--- update-hostname.sh
+++ update-hostname.sh
@@ -2,10 +2,11 @@
 
 current_hostname=$(</etc/hostname)
 printf "Current Hostname: %s\n" "$current_hostname" 
-read -p 'New Hostname: ' new_hostname
+read -p 'New Hostname (or empty to leave as is): ' new_hostname
+if [ -z "${new_hostname}" ]; then
+    mv /etc/hostname /etc/hostname.backup
+    echo "$new_hostname" > /etc/hostname
 
-mv /etc/hostname /etc/hostname.backup
-echo "$new_hostname" > /etc/hostname
-
-cp /etc/hosts /etc/hosts.backup
-sed -i 's/'$current_hostname'/'$new_hostname'/g' /etc/hosts 
+    cp /etc/hosts /etc/hosts.backup
+    sed -i 's/'$current_hostname'/'$new_hostname'/g' /etc/hosts 
+fi

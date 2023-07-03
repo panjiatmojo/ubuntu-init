0772a172b594498a13baf84ae9ef47987bf60448
Panji Tri Atmojo <panji.atmojo@gmail.com>
2023-07-03 11:37:11 +0700

    Updated prompt

--- update-hostname.sh
+++ update-hostname.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-printf "Current Hostname: %s" < /etc/hostname 
+current_hostname=$(</etc/hostname)
+printf "Current Hostname: %s\n" "$current_hostname" 
 read -p 'New Hostname: ' new_hostname
 
 mv /etc/hostname /etc/hostname.backup

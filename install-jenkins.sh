e9cf4586340ddbe1b9584554615f282caa3e4ad1
Panji Tri Atmojo <panji.atmojo@gmail.com>
2023-07-11 20:34:54 +0700

    Disable SSH Key generation

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -2,5 +2,5 @@
 
 # Create jenkins user
 adduser jenkins && \
-sudo -u jenkins bash -c './install-jenkins-sub.sh' && \
-echo 'jenkins ALL = (root) NOPASSWD: /usr/bin/git, /usr/bin/docker-compose, /usr/bin/chmod' >> /etc/sudoers.d/jenkins-levero
+# sudo -u jenkins bash -c './install-jenkins-sub.sh' && \
+echo 'jenkins ALL = (root) NOPASSWD: /usr/bin/git, /usr/bin/docker-compose, /usr/bin/chmod' >> /etc/sudoers.d/jenkins
